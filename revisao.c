c4348b7c12ac7b495a5373dbb44dec050427b466
Maria Alice A S <32019494+mariaaliceASS@users.noreply.github.com>
2025-06-06 21:44:15 -0300

    Add files via upload
    File de revisão pela metade.

---
+++ revisao.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+typedef struct {
+	char nomeProfessor[50];
+	char nomeDisciplina[50];
+	char cod[50];
+	int periodos;
+	int semestre;
+	float nota;
+	
+}disciplina;
+
+int main(){
+	disciplina cad
+	
+}
+
+void menu(){
+	switch(n):
+		case 1:
+			printf("Digite o nome da disciplina>")
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			break;
+		case 6:
+			break;
+		case 7:
+			break;
+		
+}
